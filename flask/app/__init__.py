d77ca9397df7bef108fb01bda1c7574a3dc60672
aidan mcbride <aidanmcbride@outlook.com>
2019-08-26 15:22:08 -0600

    Add env var to flask app output
    Flask app will now display the env var set in docker-compose when accessed in the browser.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello worlds"
+    # get name stored as env var
+    app_name = os.getenv("ENV_NAME")
+
+    if app_name:
+        return "Hello from {}".format(app_name)
+
+    return "Hello Worlds"
